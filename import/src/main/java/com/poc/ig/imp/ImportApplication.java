7d7d1ba284ebc5bc0c78f3b290eecc41563d978e
Gunda, Sampathi <gunsa05@ca.com>
2018-10-28 14:00:46 +0530

    Import Service

---
+++ import/src/main/java/com/poc/ig/imp/ImportApplication.java
@@ -0,0 +1,12 @@
+package com.poc.ig.imp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportApplication.class, args);
+	}
+}
